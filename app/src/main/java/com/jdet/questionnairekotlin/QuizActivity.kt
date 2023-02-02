2ae82daefaadd6004d88fe67eda1c8aaf64abb8a
Jeremy Namikaze <jeremsenpai@Jeremys-MacBook-Pro.local>
2023-02-02 10:01:03 +0100

    Add Activity

--- app/src/main/java/com/jdet/questionnairekotlin/QuizActivity.kt
+++ app/src/main/java/com/jdet/questionnairekotlin/QuizActivity.kt
@@ -2,13 +2,10 @@ package com.jdet.questionnairekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class QuizActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_quiz)
     }
-
-    fun onClickBtnPlay(view: View)
 }
